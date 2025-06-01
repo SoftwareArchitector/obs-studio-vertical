b4ea93139f9761fb94cd8fe3996eb2d3d61dae93
Oleksandr Kosenko <software.architector@gmail.com>
2025-06-01 13:42:15 -0400

    123

--- cmake/macos/helpers.cmake
+++ cmake/macos/helpers.cmake
@@ -86,26 +86,30 @@ function(set_target_properties_obs target)
       endif()
 
       get_property(obs_executables GLOBAL PROPERTY _OBS_EXECUTABLES)
+ 
+      if ("${obs_executables}") 
+        message(STATUS "Add dependencies (${target}) (${obs_executables})")
+        add_dependencies(${target} ${obs_executables})
 
-      message(STATUS "Add dependencies (${target}) (${obs_executables})")
-      add_dependencies(${target} ${obs_executables})
+        foreach(executable IN LISTS obs_executables)
+          message(STATUS "Set target xcode properties (${executable}) ($(LOCAL_APPS_DIR)/$<TARGET_BUNDLE_DIR_NAME:${target}>/Contents/MacOS)")
+          set_target_xcode_properties(${executable} PROPERTIES INSTALL_PATH
+                                      "$(LOCAL_APPS_DIR)/$<TARGET_BUNDLE_DIR_NAME:${target}>/Contents/MacOS"
+          )
 
-      foreach(executable IN LISTS obs_executables)
-        message(STATUS "Set target xcode properties (${executable}) ($(LOCAL_APPS_DIR)/$<TARGET_BUNDLE_DIR_NAME:${target}>/Contents/MacOS)")
-        set_target_xcode_properties(${executable} PROPERTIES INSTALL_PATH
-                                    "$(LOCAL_APPS_DIR)/$<TARGET_BUNDLE_DIR_NAME:${target}>/Contents/MacOS"
-        )
-
-        message(STATUS "Add custom command (${target}) (${CMAKE_COMMAND}" -E copy_if_different "$<TARGET_FILE:${executable}>) ($<TARGET_BUNDLE_CONTENT_DIR:${target}>/MacOS/)")
-        add_custom_command(
-          TARGET ${target}
-          POST_BUILD
-          COMMAND
-            "${CMAKE_COMMAND}" -E copy_if_different "$<TARGET_FILE:${executable}>"
-            "$<TARGET_BUNDLE_CONTENT_DIR:${target}>/MacOS/"
-          COMMENT "Copy ${executable} to application bundle"
-        )
-      endforeach()
+          message(STATUS "Add custom command (${target}) (${CMAKE_COMMAND}" -E copy_if_different "$<TARGET_FILE:${executable}>) ($<TARGET_BUNDLE_CONTENT_DIR:${target}>/MacOS/)")
+          add_custom_command(
+            TARGET ${target}
+            POST_BUILD
+            COMMAND
+              "${CMAKE_COMMAND}" -E copy_if_different "$<TARGET_FILE:${executable}>"
+              "$<TARGET_BUNDLE_CONTENT_DIR:${target}>/MacOS/"
+            COMMENT "Copy ${executable} to application bundle"
+          )
+        endforeach()
+      else()
+        message(STATUS "No dependencies (${target})")
+      endif()
 
       if(VIRTUALCAM_DEVICE_UUID AND VIRTUALCAM_SOURCE_UUID AND VIRTUALCAM_SINK_UUID)
         set(has_virtualcam_uuids TRUE)
